4dd5bae72ddf741bada354d604ef16b77ab9af50
aptarmy <apt.enjoy@gmail.com>
2016-12-11 20:35:54 +0700

    Tweak: add expired_date to do_action

--- library/OAuth2/Storage/Wordpressdb.php
+++ library/OAuth2/Storage/Wordpressdb.php
@@ -151,18 +151,21 @@ class Wordpressdb implements
 	 */
 	public function setAccessToken($access_token, $client_id, $user_id, $expires, $scope=null) {   
 
+		// Fomatting expired date
+		$expires = date('Y-m-d H:i:s', $expires);
+		
 		/** 
 		 * wo_set_access_token Action
-		 * Returns access_token, client_id, $user_id
+		 * Returns access_token, expired_date, client_id, $user_id
 		 * @since  3.1.9
 		 */
 		do_action('wo_set_access_token', array( 
 			'access_token' => $access_token, 
+			'expired_date' => $expires,
 			'client_id' => $client_id, 
-			'user_id' => $user_id 
+			'user_id' => $user_id
 		));
 
-		$expires = date('Y-m-d H:i:s', $expires);
 		if ($this->getAccessToken($access_token)) {
 			$stmt = $this->db->prepare("UPDATE {$this->db->prefix}oauth_access_tokens SET client_id=%s, expires=%s, user_id=%s, scope=%s where access_token=%s", array($client_id, $expires, $user_id, $scope, $access_token));
 		} else {
